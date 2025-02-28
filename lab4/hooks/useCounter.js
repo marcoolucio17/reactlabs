a715b7c727152de296d668a53b53f4a4673d77f3
Marco Lucio <marcoalucio15@gmail.com>
2025-02-27 21:19:12 -0600

    changed comments on useCounter.

--- lab4/hooks/useCounter.js
+++ lab4/hooks/useCounter.js
@@ -6,7 +6,6 @@ export const useCounter = (initialValue = 10) => {
     setCounter(counter + value);
   };
   const decrement = (value = 1) => {
-    // if ( counter === 0 ) return;
     setCounter(counter - value);
   };
   const reset = () => {
